2e94843318ff79b700d467f189227f48d044f253
Tobiáš Potoček <tobiaspotocek@gmail.com>
2015-01-29 15:16:08 +0100

    created dev stack

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        assemble: {
+            options: {
+                assets: 'public/',
+                partials: ['templates/includes/**/*.hbs'],
+                layout: ['templates/layouts/default.hbs'],
+                flatten: true
+            },
+            site: {
+                src: ['templates/*.hbs'],
+                dest: 'public'
+            }
+        },
+
+        less: {
+            options: {
+                paths: ['node_modules', 'node_modules/bootstrap/less'],
+                cleancss: true
+            },
+            default: {
+                files: {
+                    'public/styles/styles.css': 'assets/stylesheets/index.less'
+                }
+            }
+        },
+
+        browserify: {
+            devel: {
+                options: {
+                    debug: true,
+                    watch: true,
+                    keepAlive: true
+                },
+                files: {
+                    'public/scripts/scripts.js': 'assets/scripts/index.js'
+                }
+            },
+            build: {
+                options: {
+                    debug: false,
+                    transform: [['uglifyify', {global: true}]]
+                },
+                files: {
+                    'public/scripts/scripts.js': 'assets/scripts/index.js'
+                }
+            }
+        },
+
+        watch: {
+            styles: {
+                files: 'assets/stylesheets/**/*',
+                tasks: ['less:default'],
+                options: {interrupt: true}
+            },
+            templates: {
+                files: 'templates/**/*',
+                tasks: ['assemble'],
+                options: {interrupt: true}
+            }
+        },
+
+        copy: {
+            all: {
+                files: [{
+                    expand: true,
+                    cwd: 'assets/images/',
+                    src: '**',
+                    dest: 'public/images/'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('assemble');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('build', ['browserify:build', 'less', 'assemble', 'copy']);
+    grunt.registerTask('devel', ['less', 'assemble', 'copy', 'watch', 'browserify:devel']);
+};
---
+++ assets/scripts/index.js
@@ -0,0 +1,4 @@
+$ = require('jquery');
+
+$(document).ready(function () {
+});
