57fba0395f30ce26c35b3b9e980f5685f56254ec
Tobiáš Potoček <tobiaspotocek@gmail.com>
2015-03-02 23:03:13 +0100

    introduced about page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,17 +3,15 @@ var $ = require('jquery');
 window.$ = window.jQuery = $;
 
 // Add only those Bootstrap modules that I need
-// require('bootstrap/js/transition');
+require('bootstrap/js/transition');
 require('bootstrap/js/collapse');
 
 var mattyImage = require('./mattyImage');
-var clientBanner = require('./clientBanner');
 var flexibleHeader = require('./flexibleHeader');
 var greedyJumbotron = require('./greedyJumbotron');
 
 $(document).ready(function () {
     mattyImage.init();
-    // $('.client-banner').each(clientBanner);
     $('nav.navbar').each(flexibleHeader);
     $('.greedy-jumbotron').each(greedyJumbotron);
 });
